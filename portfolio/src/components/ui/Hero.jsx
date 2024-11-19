8335d9921b05a7d01db01436f23babdda6da6a5d
kimhongzhang323 <kim.hong.zhang323@gmail.com>
2024-11-19 19:39:22 +0800

    Update button

--- portfolio/src/components/ui/Hero.jsx
+++ portfolio/src/components/ui/Hero.jsx
@@ -1,6 +1,7 @@
 import { Typewriter } from 'react-simple-typewriter';
 import { useEffect, useState } from 'react';
 import { HERO_CONTENT } from '../../constants';
+import Button from './button';
 
 const Hero = () => {
     const [isMounted, setIsMounted] = useState(false);
@@ -10,34 +11,40 @@ const Hero = () => {
     }, []);
 
     return (
-        <div className="flex flex-wrap ml-14">
-            <div className="text-left">
-                <h2 className="text-2xl font-thin tracking-tight lg:text-4xl text-white mb-4">
-                    Hello, I'm
-                </h2>
-                {isMounted && (
-                    <h1 className="text-2xl font-thin tracking-tight lg:text-8xl text-white mb-4">
-                        <span>
-                            <Typewriter
-                                words={['Kim Hong Zhang']}
-                                loop={true}
-                                typeSpeed={70}
-                                deleteSpeed={50}
-                                delaySpeed={1000}
-                                cursor={true} // Adds a blinking cursor
-                                cursorStyle="|" // Customizes cursor style to a vertical bar
-                            />
-                        </span>
-                    </h1>
-                )}
-                <span className="gradient-text bg-clip-text text-3xl tracking-light text-transparent mt-6 block">
-                    AI and ML Developer
-                </span>
-                <p className="my-2 max-w-xl py-6 font-light tracking-tighter text-white leading-8">
-                    {HERO_CONTENT}
-                </p>
+        <div>
+            <div className="flex flex-wrap ml-14">
+                <div className="text-left">
+                    <h2 className="text-2xl font-thin tracking-tight lg:text-4xl text-white mb-4">
+                        Hello, I'm
+                    </h2>
+                    {isMounted && (
+                        <h1 className="text-2xl font-thin tracking-tight lg:text-8xl text-white mb-4">
+                            <span>
+                                <Typewriter
+                                    words={['Kim Hong Zhang']}
+                                    loop={true}
+                                    typeSpeed={70}
+                                    deleteSpeed={50}
+                                    delaySpeed={1000}
+                                    cursor={true} // Adds a blinking cursor
+                                    cursorStyle="|" // Customizes cursor style to a vertical bar
+                                />
+                            </span>
+                        </h1>
+                    )}
+                    <span className="gradient-text bg-clip-text text-3xl tracking-light text-transparent mt-6 block">
+                        AI and ML Developer
+                    </span>
+                    <p className="my-2 max-w-xl py-6 font-light tracking-tighter text-white leading-8">
+                        {HERO_CONTENT}
+                    </p>
+                </div>
+            </div>
+            <div className="flex justify-center">
+                <Button/>
             </div>
         </div>
+
     );
 }
 
--- portfolio/src/constants/index.js
+++ portfolio/src/constants/index.js
@@ -1,6 +1,20 @@
-
-
 export const HERO_CONTENT = `I am an AI and ML student currently in my first year, first semester. Passionate about exploring the intersection of data and intelligence, I am diving into the fundamentals of artificial intelligence and machine learning, aiming to build innovative solutions that enhance user experiences and drive technological advancements.`;
 
 export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
+export const PERSONAL_DETAILS = [
+  'Name: Kim Hong Zhang',
+  'Email: kim.hong.zhang323@gmail.com',
+  'Organisation: University of Malaya',
+  'Age: 19',
+  'Phone Number: +6012-5751521',
+];
+
+
+export const PROGRAMMING_SKILLS = [
+    { skill: 'JavaScript', level: 90 },
+    { skill: 'React', level: 85 },
+    { skill: 'Node.js', level: 80 },
+    { skill: 'MySQL', level: 20 },
+    { skill: 'MongoDB', level: 70 },
+];
